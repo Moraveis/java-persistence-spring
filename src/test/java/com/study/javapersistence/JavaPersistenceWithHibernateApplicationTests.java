662d0c84010279617db6735006000903c24da043
Joao Vitor <joaovitormoraveis@gmail.com>
2023-12-09 18:50:55 -0500

    Basic JPA and Hibernate configuration using Persistence xml

---
+++ src/test/java/com/study/javapersistence/JavaPersistenceWithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javapersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPersistenceWithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
