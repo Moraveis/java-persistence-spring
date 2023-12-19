542f1b9bf59a26d81b1569b0c718e6449cdec4c6
Joao Vitor <joaovitormoraveis@gmail.com>
2023-12-18 20:09:22 -0500

    Table per concrete class with unions

--- src/main/java/com/study/javapersistence/domain/BillingDetails.java
+++ src/main/java/com/study/javapersistence/domain/BillingDetails.java
@@ -1,11 +1,14 @@
 package com.study.javapersistence.domain;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.validation.constraints.NotNull;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class BillingDetails {
 
     @Id
